fb6f05aca5c5f0712354d57d7e3de9ad6e524522
Nick Booker <NMBooker@gmail.com>
2012-05-21 12:11:58 +0100

    Added test for passempty

--- test/test_passnil.rb
+++ test/test_passnil.rb
@@ -10,4 +10,11 @@ class TestPassnil < Test::Unit::TestCase
     assert_equal nil.passnil('nothing here') { |x| x }, 'nothing here'
     assert_equal 'abc'.passnil { |x| x.upcase }, 'ABC'
   end
+
+  def test_passempty
+    assert_nil nil.passempty {|x| x }
+    assert_nil ''.passempty { |x| x }
+    assert_equal '', ''.passempty('') { |x| x }
+    assert_equal 'ABC', 'abc'.passempty('') { |x| x.upcase }
+  end
 end
